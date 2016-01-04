11704f05dfb417cbffdcf8b1d9b39592cff331b0
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2016-01-04 10:47:54 +0000

    Update gisOps.py

--- gisOps.py
+++ gisOps.py
@@ -1,12 +1,18 @@
 import gisHelper
 import requests
+import gisHelper
 # A sample script to take a diagram from Geodesign Hub, buffer it and send it back as a new diagram using the API. 
 
 USER_TOKEN = ''
+PROJECT_ID = ''
 
 class APIConnector():
 	''' A class to connect to the GDH service'''
 	pass
+
+
+myGISHelper = diagGISHelper.GISFactory()
+transformedFC = myGISHelper.processGeoms(geoms, operation, distance, units)
 
 
 # get a diagram 
