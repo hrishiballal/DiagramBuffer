d57cae890adedbe16b0633c06d3fc2b8346e8479
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2016-01-19 14:55:57 +0000

    Added new GDH Client file.

--- bufferOps.py
+++ bufferOps.py
@@ -1,23 +1,22 @@
-import gisHelper, requests, json, GDHAPIHelper
+import gisHelper, requests, json, GeodesignHub
 # A sample script to take a diagram from Geodesign Hub, buffer it and send
 # it back as a new diagram using the API.
 #
-
 
 if __name__ == "__main__":
 	diagID = 32 # diagram to be downloaded
 	operation = 'buffersubtract' # GIS operation
 	distance = 100 # distance of buffer
 	units = 'm' # units
-	myAPIHelper = GDHAPIHelper.APIHelper()
+	myAPIHelper = GeodesignHub.GeodesignHubClient(url = 'http://local.dev:8000/api/v1/', project_id='62ead880b1592bc0', token='5d72a5465bc8a61bb6dd02457cbf97150735bfbf')
 	# the id of the diagram that needs to be transformed as a part of GIS operation.
-	r = myAPIHelper.getDiagramGeoms(diagID)
-	print r.status_code
+	r = myAPIHelper.get_diagram_geoms(diagID)
+
 	if r.status_code == 200:
 		op = json.loads(r.text)
 		geoms = op['geojson']
 		myGISHelper = gisHelper.GISFactory()
 		transformedFC = myGISHelper.processGeoms(geoms, operation, distance, units)
 		# print json.dumps(transformedFC)
-		upload = myAPIHelper.submitUpload(geoms = transformedFC, projectorpolicy= 'policy',featuretype = 'polygon', description= 'Subtracted buffer', reqid = 2 )
+		upload = myAPIHelper.post_as_diagram(geoms = transformedFC, projectorpolicy= 'policy',featuretype = 'polygon', description= 'Subtracted buffer', reqid = 2 )
 		print upload.text
